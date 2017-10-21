56cb7d5c53de1291d57cae0add4232cff127c0da
Pavel <pavel.shering@gmail.com>
2017-10-21 16:14:50 -0400

    displaying direction for robot movement and heading

--- Assignment1/assig1.m
+++ Assignment1/assig1.m
@@ -1,18 +1,19 @@
 clear all; clc;
-
+close all;
 % robots sizing 
-L = 30; % [cm]
-r = 25; % [cm]
+L = 0.5; % [m]
+r = 1; % [m]
 f = 10; % [Hz]
 
 % kinematic model
-dt = 0.01;% [s] timestep 
-x0 = [0; 0; 0] % [[m/s] [m/s] [rad/s]] intial state
-u = [1; 1; 1] % [[rad/s] [rad/s] [rad/s]] inputs
+dt = 0.001;% [s] timestep 
+x0 = [0; 0; pi()] % [[m/s] [m/s] [rad/s]] intial state
+u = [-1.5; 2; 1] % [[rad/s] [rad/s] [rad/s]] inputs
+u = [2; 0; 0]
 
 % noise model 
 
-n = 10; % number of samples
+n = 10000; % number of samples
 figure(1); clf; hold on;
 
 for i = 1:n
@@ -28,10 +29,18 @@ for i = 1:n
 %     
 %     plot([v1, v2, v3], '*')
     % dynamics
-    x1 = x0 + [ (r/3)*dt*( -u(1)*cos(x0(3)) + u(2)*cos(pi()/3-x0(3)) + u(3)*cos(pi()/3+x0(3)) ) ;
-                (r/3)*dt*( u(1)*sin(x0(3)) + u(2)*sin(pi()/3-x0(3)) - u(3)*sin(pi()/3+x0(3)) );
-                (r/(3*L))*dt*(u(1)+u(2)+u(3))];
-    x0 = x1        
+    x_record(:,i) = x0;
+    v_record(:,i) = [(r*2/3)*( -u(1)*cos(x0(3)) + u(2)*cos(pi()/3-x0(3)) + u(3)*cos(pi()/3+x0(3)) );
+                     (r*2/3)*( u(1)*sin(x0(3)) + u(2)*sin(pi()/3-x0(3)) - u(3)*sin(pi()/3+x0(3)) );
+                     r/(3*L)*(u(1)+u(2)+u(3))];
+                 
+    x1 = x0 + [ (r*2/3)*( -u(1)*cos(x0(3)) + u(2)*cos(pi()/3-x0(3)) + u(3)*cos(pi()/3+x0(3)) )*dt;
+                (r*2/3)*( u(1)*sin(x0(3)) + u(2)*sin(pi()/3-x0(3)) - u(3)*sin(pi()/3+x0(3)) )*dt;
+                (r/(3*L))*(u(1)+u(2)+u(3))*dt];
+    x0 = x1; 
 end
 
+% a = sqrt(v_record(1,:).^2 + v_record(2,:).^2)
 
+quiver(x_record(1,:),x_record(2,:),v_record(1,:),v_record(2,:));
+quiver(x_record(1,:),x_record(2,:),sin(x_record(3,:)),cos(x_record(3,:)),'r*');
