4cb18bab44386b4990c40ffd1ee6b8042ade8c69
llustgar <llustgar@gear21.CHORUS.UWATERLOO.CA>
2017-10-25 13:13:05 -0400

    working part 1 and 2 with disturbances

--- Assignment1/assig1.m
+++ Assignment1/assig1.m
@@ -6,47 +6,44 @@ r = 0.25; % [m]
 f = 10; % [Hz]
 
 % kinematic model
-dt = 0.001;% [s] timestep 
-x0 = [0; 0; 0] % [[m/s] [m/s] [rad/s]] intial state
-%u = [-1.5; 2; 1] % [[rad/s] [rad/s] [rad/s]] inputs
- u = [2; 2; 0]
+dt = 0.1;% [s] timestep (update rate)
+x0 = [0; 0; 0]; % [[m/s] [m/s] [rad/s]] intial state
+u = [-1.5; 2; 1]; % [[rad/s] [rad/s] [rad/s]] inputs
+% u = [2; 2; 0];
+%u = [2; -1.167612497392722; 0]; % 2m radius circle inputs 
 
 % noise model 
 
 n = 15 / dt; % number of samples for 15 second simulation
 figure(1); clf; hold on;
 
-for i = 1:n
-    u(3) = 2*(n - i) / n;
-%incorrect velocity equations, look at the bottom equations.
-%     theta = i;
-%     v1 = - u(1)*cos(x0(3)) + j*(u(1)*sin(x0(3)))
-%     v2 = u(2)*cos(1/3*pi()-x0(3)) + j*(u(2)*sin(pi()/3-x0(3)))
-%     v3 = u(3)*cos(1/3*pi()+x0(3)) - j* (u(3)*sin(1/3*pi()+x0(3)))
-%     
-%    u = [v1;v2;v3]/r;
+x_record = zeros(3, n);
+v_record = zeros(3, n);
 
-    %     abs (v1)
-%     abs (v2)
-%     abs (v3)
-%     
-%     x0(3) = x0(3) + 0.1;
-%     
-%     plot([v1, v2, v3], '*')
+omega_variance = 0.1 / 180 * pi();
+
+for i = 1:n
+    %u(3) = 2*(n - i) / n;
+
     % dynamics
+    v_x = (r*2/3) * (-u(1)*cos(x0(3)) + u(2)*cos(pi()/3-x0(3)) + u(3)*cos(pi()/3+x0(3)));
+    v_y = (r*2/3) * (u(1)*sin(x0(3)) + u(2)*sin(pi()/3-x0(3)) - u(3)*sin(pi()/3+x0(3)));
+    omega = r/(3*L) * (u(1)+u(2)+u(3));
+    
+    rad = sqrt(v_x*v_x + v_y*v_y) / omega;
+    d = rad * 2
+    
     x_record(:,i) = x0;
-    v_record(:,i) = [(r*2/3)*( -u(1)*cos(x0(3)) + u(2)*cos(pi()/3-x0(3)) + u(3)*cos(pi()/3+x0(3)) );
-                     (r*2/3)*( u(1)*sin(x0(3)) + u(2)*sin(pi()/3-x0(3)) - u(3)*sin(pi()/3+x0(3)) );
-                     r/(3*L)*(u(1)+u(2)+u(3))];
+    v_record(:,i) = [v_x;
+                     v_y;
+                     omega];
                  
-    x1 = x0 + [ (r*2/3)*( -u(1)*cos(x0(3)) + u(2)*cos(pi()/3-x0(3)) + u(3)*cos(pi()/3+x0(3)) )*dt;
-                (r*2/3)*( u(1)*sin(x0(3)) + u(2)*sin(pi()/3-x0(3)) - u(3)*sin(pi()/3+x0(3)) )*dt;
-                (r/(3*L))*(u(1)+u(2)+u(3))*dt];
+    x1 = x0 + [ v_x * dt; v_y * dt; omega * dt] + [normrnd(0,0.01); normrnd(0,0.01); normrnd(0,omega_variance)];
     x0 = x1; 
 end
 
 % a = sqrt(v_record(1,:).^2 + v_record(2,:).^2)
-samples_per_draw = 100;
+samples_per_draw = 1;
 trimmed_x_rec = x_record(:,1:samples_per_draw:end);
 trimmed_v_rec = v_record(:,1:samples_per_draw:end);
 
---
+++ Assignment1/solving_for_radius.m
@@ -0,0 +1,15 @@
+syms v_x v_y  u2 omega radius
+
+u1 = 1;
+u3 = 0;
+
+radius = 2;
+
+v_x = (r*2/3) * (-u1*cos(0) + u2*cos(pi()/3-0) + u3*cos(pi()/3+0));
+v_y = (r*2/3) * (u1*sin(0) + u2*sin(pi()/3-0) - u3*sin(pi()/3+0));
+omega = r/(3*L) * (u1+u2+u3);
+    
+
+ eqn1 = sqrt(v_x ^2 + v_y^2)/omega - radius
+
+fprintf ('%.15ld',double(solve (eqn1, u2)));
