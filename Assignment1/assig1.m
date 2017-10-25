87da36e98391c967beaefd8cc009a551521e6d90
Pavel <pavel.shering@gmail.com>
2017-10-25 13:52:21 -0400

    adding measurement model with std deviations

--- Assignment1/assig1.m
+++ Assignment1/assig1.m
@@ -19,24 +19,29 @@ figure(1); clf; hold on;
 
 x_record = zeros(3, n);
 v_record = zeros(3, n);
+y_record = zeros(3, n);
 
 omega_variance = 0.1 / 180 * pi();
 
 for i = 1:n
     %u(3) = 2*(n - i) / n;
+    %measurement
+    y = x0 + [normrnd(0,0.5); normrnd(0,0.5); normrnd(-9.7 * 180/pi(),10 * 180/pi())];
+
 
     % dynamics
-    v_x = (r*2/3) * (-u(1)*cos(x0(3)) + u(2)*cos(pi()/3-x0(3)) + u(3)*cos(pi()/3+x0(3)));
-    v_y = (r*2/3) * (u(1)*sin(x0(3)) + u(2)*sin(pi()/3-x0(3)) - u(3)*sin(pi()/3+x0(3)));
+    v_x = (r*2/3) * (-u(1)*cos(x0(3)) + u(2)*cos(pi()/3+x0(3)) + u(3)*cos(pi()/3-x0(3)) );
+    v_y = (r*2/3) * (u(1)*sin(x0(3))  - u(2)*sin(pi()/3+x0(3)) + u(3)*sin(pi()/3-x0(3)));
     omega = r/(3*L) * (u(1)+u(2)+u(3));
     
     rad = sqrt(v_x*v_x + v_y*v_y) / omega;
-    d = rad * 2
+    d = rad * 2;
     
     x_record(:,i) = x0;
     v_record(:,i) = [v_x;
                      v_y;
                      omega];
+    y_record(:,i) = y;
                  
     x1 = x0 + [ v_x * dt; v_y * dt; omega * dt] + [normrnd(0,0.01); normrnd(0,0.01); normrnd(0,omega_variance)];
     x0 = x1; 
@@ -46,6 +51,10 @@ end
 samples_per_draw = 1;
 trimmed_x_rec = x_record(:,1:samples_per_draw:end);
 trimmed_v_rec = v_record(:,1:samples_per_draw:end);
+trimmed_y_rec = y_record(:,1:samples_per_draw:end);
+
 
 quiver(trimmed_x_rec(1,:),trimmed_x_rec(2,:),trimmed_v_rec(1,:),trimmed_v_rec(2,:));
 quiver(trimmed_x_rec(1,:),trimmed_x_rec(2,:),sin(trimmed_x_rec(3,:)),cos(trimmed_x_rec(3,:)),'r*');
+
+quiver(trimmed_y_rec(1,:),trimmed_y_rec(2,:),zeros(n),zeros(n),'g*');
